bf0c0b2798beed923af27f62dfedcc18a3de8c3c
Iván Gallego Torres <id.gallego16@iesdoctorbalmis.com>
2019-10-23 15:27:16 +0200

    Proyecto parcialmente terminado

--- MemeGenerator/CheckBoxConverter.cs
+++ MemeGenerator/CheckBoxConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,22 @@ namespace MemeGenerator
 {
     class CheckBoxConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool valor = (bool)value;
+            if (valor)
+            {
+                return 3;
+            }
+            else
+            {
+                return 0;
+            }
+        }
 
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
